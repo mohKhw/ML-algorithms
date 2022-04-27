0d2de4b5b8a5ccb40afafe9fcdfa7453348d37b7
mohKhw <mohammadkhwildi@gmail.com>
2022-04-27 07:54:05 +0300

    first commit

---
+++ image_processing.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def cross_correlation(image, filter, mode='valid'):
+    return 111
+
+def convolution(image, filter, mode='valid'):
+    return 110
---
+++ machine_learning.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def preprocess_classification_dataset():
+    return 999
+
+def knn_classification(X_train, y_train, x_new, k=5):
+    return 998
+
+def logistic_regression_training(X_train, y_train, alpha=0.01, max_iters=5000, random_seed=1):
+    return 997
+
+def logistic_regression_prediction(X, weights, threshold=0.5):
+    return 996
+
+def model_selection_and_evaluation(alpha=0.01, max_iters=5000, random_seed=1, threshold=0.5):
+    return 995

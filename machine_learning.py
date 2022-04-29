6f58fcb1adca2e0b6a5cc30eae1dfa13bbd34151
mohKhw <mohammadkhwildi@gmail.com>
2022-04-29 05:51:12 +0300

    preprocess_classif commit

--- machine_learning.py
+++ machine_learning.py
@@ -1,7 +1,26 @@
 import numpy as np
+import pandas as pd
 
 def preprocess_classification_dataset():
-    return 999
+    ##train##
+    csv_reader_train = pd.read_csv('train.csv')
+    train_feat_df = csv_reader_train.iloc[:,:-1] # grab all columns except the last one 
+    train_output = csv_reader_train[['output']]
+    X_train = train_feat_df.values
+    y_train = train_output.values
+    ##val##
+    csv_reader_val = pd.read_csv('val.csv')
+    val_feat_df = csv_reader_val.iloc[:,:-1] # grab all columns except the last one 
+    val_output = csv_reader_val[['output']]
+    X_val = val_feat_df.values
+    y_val = val_output.values
+    ##test##
+    csv_reader_test = pd.read_csv('test.csv')
+    test_feat_df = csv_reader_test.iloc[:,:-1] # grab all columns except the last one 
+    test_output = csv_reader_test[['output']]
+    X_test = test_feat_df.values
+    y_test = test_output.values
+    return X_train, y_train, X_val, y_val, X_test, y_test
 
 def knn_classification(X_train, y_train, x_new, k=5):
     return 998
